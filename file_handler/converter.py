413e8df9769779bf6e2a5a2b5204c4a2bbeb1764
MurugesMarvel <murugeshmarvel95@zoho.com>
2017-04-05 20:31:29 +0530

    working on converter

---
+++ file_handler/converter.py
@@ -0,0 +1,27 @@
+
+import os
+import converter_engine as converter_obj
+
+class convert:
+    def convert_file(self, filepath,save_path,from_file,to_file):
+        if from_file == 'doc':
+            if to_file == 'pdf':
+                converter_obj.doc2pdf(filepath,save_path)
+
+            if to_file == 'txt':
+                converter_obj.doc2txt(filepath,save_path)
+
+            else:
+                print 'doc can be converted only to pdf,text using this program'
+
+        elif from_file == 'pdf':
+            if to_file == 'doc':
+                converter_obj.pdf2doc(filepath,save_path)
+            elif to_file == 'txt':
+                converter_obj.pdf2txt(filepath,save_path)
+
+        elif from_file == 'txt':
+            if to_file == 'doc':
+                converter_obj.txt2doc(filepath,save_path)
+            elif to_file == 'pdf':
+                converter_obj.txt2pdf(filepath,save_path)
